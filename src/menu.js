409b751297bee20f127a8be051b16e968085f1a9
Arnauld <157052334+TWork1@users.noreply.github.com>
2024-03-28 08:59:56 -0600

    impoterted

--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,5 @@
 import './menu.css'
+import './imge/Bread.jpg' 
 const content= document.querySelector('#content');
 export const menu = ()=>{
     while(content.hasChildNodes()){
@@ -11,7 +12,7 @@ const bread = document.createElement('div');
 bread.classList.add('bread');
 const breadimg = document.createElement('div');
 breadimg.classList.add('breadimg')
-breadimg.innerHTML ='<img src="../src/imge/Bread.jpg">'
+breadimg.innerHTML ='<img src="imge/Bread.jpg">'
 const breadtext = document.createElement('div');
 breadtext.classList.add('breadtext')
 breadtext.textContent = "Indulge in the timeless allure of our Homemade Bread, a culinary masterpiece that embodies the essence of comfort and tradition. Crafted with care and passion in our kitchen, each loaf is a celebration of artisanal baking at its finest."
@@ -23,7 +24,7 @@ const coq = document.createElement('div');
 coq.classList.add('coq');
 const coqimg = document.createElement('div');
 coqimg.classList.add('coqimg')
-coqimg.innerHTML ='<img src="../src/imge/coq.jpg">'
+coqimg.innerHTML ='<img src="imge/coq.jpg">'
 const coqtext = document.createElement('div');
 coqtext.classList.add('coqtext')
 coqtext.textContent = 'Savor the exquisite flavors of France with our Coq au Vin. Tender chicken, simmered in a rich red wine sauce with pearl onions and aromatic herbs, delivers a symphony of savory indulgence.'
@@ -35,7 +36,7 @@ const wine = document.createElement('div');
 wine.classList.add('wine');
 const wineimg = document.createElement('div');
 wineimg.classList.add('wineimg')
-wineimg.innerHTML ='<img src="../src/imge/wine.jpeg">'
+wineimg.innerHTML ='<img src="imge/wine.jpeg">'
 const winetext = document.createElement('div');
 winetext.classList.add('winetext')
 winetext.textContent = 'Elevate your palate with our Apple Cider Wine, a crisp and refreshing libation crafted from the finest handpicked apples. Experience the perfect harmony of sweet and tart notes, culminating in a delightful burst of autumnal flavors with every sip.'
